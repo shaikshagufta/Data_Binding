2dcae78876c96ce9617b33065610b0b7a80c3d90
sshagufta3 <shaikshagufta99@gmail.com>
2022-04-25 14:56:08 +0530

    DataBinding with ViewModel & LiveData

--- app/src/main/java/com/example/databinding/PersonViewModel.kt
+++ app/src/main/java/com/example/databinding/PersonViewModel.kt
@@ -3,6 +3,10 @@ package com.example.databinding
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class PersonViewModel :ViewModel() {
 
@@ -13,4 +17,19 @@ class PersonViewModel :ViewModel() {
     //observes the observable
     val personLiveData : LiveData<Person> = _personLiveData
     //we want to use its constructor
+
+    //
+    init {
+        //we want do suspension work on dispatchers IO
+        viewModelScope.launch(Dispatchers.IO) {//since Dispatchers.IO is a background thread we use postValue to post it to the main thread
+            //Thread.sleep(3000)//would delay the whole thread
+            delay(3000)//will delay only the current coroutine
+            _personLiveData.postValue(Person("Person 2 ", 15, R.drawable.ic_emoji))
+            delay(2000)
+            _personLiveData.postValue(Person("Person 3 ", 60, R.drawable.ic_emoji))
+            delay(2000)
+            _personLiveData.postValue(Person("Person 4 ", 100, R.drawable.ic_emoji))
+        }
+    }
+
 }
